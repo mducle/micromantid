29191a08ab8522522cfbce66e2c6235234a90546
Duc Le <duc.le@stfc.ac.uk>
2025-04-11 14:04:08 +0100

    Add unused source in C++ modules commented out

--- src/api.cmake
+++ src/api.cmake
@@ -4,63 +4,119 @@ set(API_SRC_FILES
     ../mantid/Framework/API/src/AlgorithmExecuteProfile.cpp
     ../mantid/Framework/API/src/AlgorithmFactory.cpp
     ../mantid/Framework/API/src/AlgorithmFactoryObserver.cpp
+#   ../mantid/Framework/API/src/AlgorithmHasProperty.cpp
     ../mantid/Framework/API/src/AlgorithmHistory.cpp
     ../mantid/Framework/API/src/AlgorithmManager.cpp
     ../mantid/Framework/API/src/AlgorithmObserver.cpp
+#   ../mantid/Framework/API/src/AlgorithmProperties.cpp
     ../mantid/Framework/API/src/AlgorithmProperty.cpp
+#   ../mantid/Framework/API/src/AlgorithmRuntimeProps.cpp
     ../mantid/Framework/API/src/AlgoTimeRegister.cpp
     ../mantid/Framework/API/src/AnalysisDataService.cpp
     ../mantid/Framework/API/src/AnalysisDataServiceObserver.cpp
+#   ../mantid/Framework/API/src/ArchiveSearchFactory.cpp
     ../mantid/Framework/API/src/Axis.cpp
     ../mantid/Framework/API/src/BinEdgeAxis.cpp
+#   ../mantid/Framework/API/src/BoostOptionalToAlgorithmProperty.cpp
     ../mantid/Framework/API/src/BoxController.cpp
+#   ../mantid/Framework/API/src/BoxControllerSettingsAlgorithm.cpp
+#   ../mantid/Framework/API/src/CatalogManager.cpp
+#   ../mantid/Framework/API/src/CatalogSession.cpp
     ../mantid/Framework/API/src/Citation.cpp
     ../mantid/Framework/API/src/Column.cpp
     ../mantid/Framework/API/src/ColumnFactory.cpp
-    ../mantid/Framework/API/src/CompositeFunction.cpp
     ../mantid/Framework/API/src/CommonBinsValidator.cpp
+#   ../mantid/Framework/API/src/CompositeCatalog.cpp
+#   ../mantid/Framework/API/src/CompositeDomainMD.cpp
+    ../mantid/Framework/API/src/CompositeFunction.cpp
     ../mantid/Framework/API/src/ConstraintFactory.cpp
+#   ../mantid/Framework/API/src/CoordTransform.cpp
+#   ../mantid/Framework/API/src/CostFunctionFactory.cpp
     ../mantid/Framework/API/src/DataProcessorAlgorithm.cpp
-    ../mantid/Framework/API/src/DeprecatedAlias.cpp
     ../mantid/Framework/API/src/DeprecatedAlgorithm.cpp
+    ../mantid/Framework/API/src/DeprecatedAlias.cpp
+#   ../mantid/Framework/API/src/DetectorSearcher.cpp
+#   ../mantid/Framework/API/src/DomainCreatorFactory.cpp
+#   ../mantid/Framework/API/src/EnabledWhenWorkspaceIsType.cpp
+#   ../mantid/Framework/API/src/EqualBinSizesValidator.cpp
     ../mantid/Framework/API/src/ExperimentInfo.cpp
     ../mantid/Framework/API/src/Expression.cpp
-    ../mantid/Framework/API/src/FileProperty.cpp
-    ../mantid/Framework/API/src/FileLoaderRegistry.cpp
+#   ../mantid/Framework/API/src/FileBackedExperimentInfo.cpp
     ../mantid/Framework/API/src/FileFinder.cpp
+    ../mantid/Framework/API/src/FileLoaderRegistry.cpp
+    ../mantid/Framework/API/src/FileProperty.cpp
     ../mantid/Framework/API/src/FrameworkManager.cpp
+#   ../mantid/Framework/API/src/FuncMinimizerFactory.cpp
     ../mantid/Framework/API/src/FunctionDomain1D.cpp
+#   ../mantid/Framework/API/src/FunctionDomainGeneral.cpp
+#   ../mantid/Framework/API/src/FunctionDomainMD.cpp
     ../mantid/Framework/API/src/FunctionFactory.cpp
+#   ../mantid/Framework/API/src/FunctionGenerator.cpp
+    ../mantid/Framework/API/src/FunctionParameterDecorator.cpp
     ../mantid/Framework/API/src/FunctionProperty.cpp
     ../mantid/Framework/API/src/FunctionValues.cpp
-    ../mantid/Framework/API/src/FunctionParameterDecorator.cpp
+#   ../mantid/Framework/API/src/GridDomain.cpp
+#   ../mantid/Framework/API/src/GridDomain1D.cpp
+#   ../mantid/Framework/API/src/GroupingLoader.cpp
+#   ../mantid/Framework/API/src/HistoWorkspace.cpp
     ../mantid/Framework/API/src/HistogramValidator.cpp
     ../mantid/Framework/API/src/HistoryItem.cpp
     ../mantid/Framework/API/src/HistoryView.cpp
+#   ../mantid/Framework/API/src/IDomainCreator.cpp
+#   ../mantid/Framework/API/src/IEventList.cpp
     ../mantid/Framework/API/src/IEventWorkspace.cpp
+#   ../mantid/Framework/API/src/IFuncMinimizer.cpp
     ../mantid/Framework/API/src/IFunction.cpp
     ../mantid/Framework/API/src/IFunction1D.cpp
+#   ../mantid/Framework/API/src/IFunction1DSpectrum.cpp
+#   ../mantid/Framework/API/src/IFunctionGeneral.cpp
+#   ../mantid/Framework/API/src/IFunctionMD.cpp
+#   ../mantid/Framework/API/src/IFunctionMW.cpp
+#   ../mantid/Framework/API/src/IJournal.cpp
+#   ../mantid/Framework/API/src/ILatticeFunction.cpp
+    ../mantid/Framework/API/src/IMDEventWorkspace.cpp
+    ../mantid/Framework/API/src/IMDHistoWorkspace.cpp
+    ../mantid/Framework/API/src/IMDIterator.cpp
+    ../mantid/Framework/API/src/IMDWorkspace.cpp
+#   ../mantid/Framework/API/src/IPawleyFunction.cpp
+    ../mantid/Framework/API/src/IPeakFunction.cpp
+    ../mantid/Framework/API/src/IPeaksWorkspace.cpp
+#   ../mantid/Framework/API/src/IPowderDiffPeakFunction.cpp
+#   ../mantid/Framework/API/src/ISISInstrumentDataCache.cpp
+#   ../mantid/Framework/API/src/ISISRunLogs.cpp
+    ../mantid/Framework/API/src/ISpectrum.cpp
+    ../mantid/Framework/API/src/ITableWorkspace.cpp
+    ../mantid/Framework/API/src/ImmutableCompositeFunction.cpp
+#   ../mantid/Framework/API/src/ImplicitFunctionFactory.cpp
+#   ../mantid/Framework/API/src/ImplicitFunctionParameterParserFactory.cpp
+#   ../mantid/Framework/API/src/ImplicitFunctionParserFactory.cpp
+#   ../mantid/Framework/API/src/IncreasingAxisValidator.cpp
+#   ../mantid/Framework/API/src/IndexProperty.cpp
+#   ../mantid/Framework/API/src/IndexTypeProperty.cpp
     ../mantid/Framework/API/src/InstrumentDataService.cpp
     ../mantid/Framework/API/src/InstrumentFileFinder.cpp
     ../mantid/Framework/API/src/InstrumentValidator.cpp
-    ../mantid/Framework/API/src/ImmutableCompositeFunction.cpp
-    ../mantid/Framework/API/src/IMDIterator.cpp
-    ../mantid/Framework/API/src/IMDEventWorkspace.cpp
-    ../mantid/Framework/API/src/IMDHistoWorkspace.cpp
-    ../mantid/Framework/API/src/IMDWorkspace.cpp
-    ../mantid/Framework/API/src/IPeakFunction.cpp
-    ../mantid/Framework/API/src/IPeaksWorkspace.cpp
-    ../mantid/Framework/API/src/ITableWorkspace.cpp
-    ../mantid/Framework/API/src/ISpectrum.cpp
+#   ../mantid/Framework/API/src/JointDomain.cpp
+#   ../mantid/Framework/API/src/LatticeDomain.cpp
+#   ../mantid/Framework/API/src/LinearScale.cpp
+#   ../mantid/Framework/API/src/LiveListener.cpp
+#   ../mantid/Framework/API/src/LiveListenerFactory.cpp
     ../mantid/Framework/API/src/LogManager.cpp
+#   ../mantid/Framework/API/src/LogarithmScale.cpp
+    ../mantid/Framework/API/src/MDFrameValidator.cpp
+    ../mantid/Framework/API/src/MDGeometry.cpp
     ../mantid/Framework/API/src/MatrixWorkspace.cpp
     ../mantid/Framework/API/src/MatrixWorkspaceMDIterator.cpp
-    ../mantid/Framework/API/src/MDGeometry.cpp
-    ../mantid/Framework/API/src/MDFrameValidator.cpp
+#   ../mantid/Framework/API/src/MuParserUtils.cpp
     ../mantid/Framework/API/src/MultiDomainFunction.cpp
+#   ../mantid/Framework/API/src/MultiPeriodGroupAlgorithm.cpp
+#   ../mantid/Framework/API/src/MultiPeriodGroupWorker.cpp
     ../mantid/Framework/API/src/MultipleExperimentInfos.cpp
     ../mantid/Framework/API/src/MultipleFileProperty.cpp
     ../mantid/Framework/API/src/NexusFileLoader.cpp
+#   ../mantid/Framework/API/src/NotebookBuilder.cpp
+#   ../mantid/Framework/API/src/NotebookWriter.cpp
+#   ../mantid/Framework/API/src/NullCoordTransform.cpp
     ../mantid/Framework/API/src/NumericAxis.cpp
     ../mantid/Framework/API/src/NumericAxisValidator.cpp
     ../mantid/Framework/API/src/OrientedLatticeValidator.cpp
@@ -69,25 +125,36 @@ set(API_SRC_FILES
     ../mantid/Framework/API/src/ParameterTie.cpp
     ../mantid/Framework/API/src/PeakFunctionIntegrator.cpp
     ../mantid/Framework/API/src/PreviewManager.cpp
-    ../mantid/Framework/API/src/Projection.cpp
     ../mantid/Framework/API/src/Progress.cpp
+    ../mantid/Framework/API/src/Projection.cpp
     ../mantid/Framework/API/src/PropertyWithValue.cpp
     ../mantid/Framework/API/src/RawCountValidator.cpp
-    ../mantid/Framework/API/src/ResizeRectangularDetectorHelper.cpp
-    ../mantid/Framework/API/src/RegionSelectorObserver.cpp
-    ../mantid/Framework/API/src/Run.cpp
     ../mantid/Framework/API/src/RefAxis.cpp
+    ../mantid/Framework/API/src/RegionSelectorObserver.cpp
+    ../mantid/Framework/API/src/ResizeRectangularDetectorHelper.cpp
+    ../mantid/Framework/API/src/Run.cpp
     ../mantid/Framework/API/src/Sample.cpp
+#   ../mantid/Framework/API/src/SampleShapeValidator.cpp
+#   ../mantid/Framework/API/src/SampleValidator.cpp
+#   ../mantid/Framework/API/src/ScopedWorkspace.cpp
+#   ../mantid/Framework/API/src/ScriptBuilder.cpp
+#   ../mantid/Framework/API/src/ScriptRepository.cpp
+#   ../mantid/Framework/API/src/ScriptRepositoryFactory.cpp
+#   ../mantid/Framework/API/src/SingleCountValidator.cpp
     ../mantid/Framework/API/src/SpectraAxis.cpp
     ../mantid/Framework/API/src/SpectraAxisValidator.cpp
     ../mantid/Framework/API/src/SpectrumDetectorMapping.cpp
     ../mantid/Framework/API/src/SpectrumInfo.cpp
     ../mantid/Framework/API/src/TableRow.cpp
     ../mantid/Framework/API/src/TextAxis.cpp
-    ../mantid/Framework/API/src/WorkspaceHistory.cpp
+#   ../mantid/Framework/API/src/TransformScaleFactory.cpp
     ../mantid/Framework/API/src/Workspace.cpp
     ../mantid/Framework/API/src/WorkspaceFactory.cpp
     ../mantid/Framework/API/src/WorkspaceGroup.cpp
+#   ../mantid/Framework/API/src/WorkspaceHasDxValidator.cpp
+    ../mantid/Framework/API/src/WorkspaceHistory.cpp
+#   ../mantid/Framework/API/src/WorkspaceNearestNeighbourInfo.cpp
+#   ../mantid/Framework/API/src/WorkspaceNearestNeighbours.cpp
     ../mantid/Framework/API/src/WorkspaceOpOverloads.cpp
     ../mantid/Framework/API/src/WorkspaceProperty.cpp
     ../mantid/Framework/API/src/WorkspacePropertyUtils.cpp
--- src/kernel.cmake
+++ src/kernel.cmake
@@ -1,52 +1,75 @@
 set(KERNEL_SRC_FILES
-    ../mantid/Framework/Kernel/src/Atom.cpp
-    ../mantid/Framework/Kernel/src/AttenuationProfile.cpp
-    ../mantid/Framework/Kernel/src/ArrayProperty.cpp
+#   ../mantid/Framework/Kernel/src/ANN_complete.cpp
     ../mantid/Framework/Kernel/src/ArrayBoundedValidator.cpp
     ../mantid/Framework/Kernel/src/ArrayLengthValidator.cpp
     ../mantid/Framework/Kernel/src/ArrayOrderedPairsValidator.cpp
+    ../mantid/Framework/Kernel/src/ArrayProperty.cpp
+    ../mantid/Framework/Kernel/src/Atom.cpp
+    ../mantid/Framework/Kernel/src/AttenuationProfile.cpp
+#   ../mantid/Framework/Kernel/src/BinFinder.cpp
+#   ../mantid/Framework/Kernel/src/BinaryStreamReader.cpp
+#   ../mantid/Framework/Kernel/src/BinaryStreamWriter.cpp
+#   ../mantid/Framework/Kernel/src/CPUTimer.cpp
     ../mantid/Framework/Kernel/src/CatalogInfo.cpp
     ../mantid/Framework/Kernel/src/ChecksumHelper.cpp
-    ../mantid/Framework/Kernel/src/ConfigService.cpp
+    ../mantid/Framework/Kernel/src/CompositeValidator.cpp
     ../mantid/Framework/Kernel/src/ConfigObserver.cpp
     ../mantid/Framework/Kernel/src/ConfigPropertyObserver.cpp
-    ../mantid/Framework/Kernel/src/CompositeValidator.cpp
+    ../mantid/Framework/Kernel/src/ConfigService.cpp
     ../mantid/Framework/Kernel/src/DataItem.cpp
     ../mantid/Framework/Kernel/src/DateAndTime.cpp
     ../mantid/Framework/Kernel/src/DateAndTimeHelpers.cpp
+#   ../mantid/Framework/Kernel/src/DateTimeValidator.cpp
+#   ../mantid/Framework/Kernel/src/DateValidator.cpp
     ../mantid/Framework/Kernel/src/DeltaEMode.cpp
     ../mantid/Framework/Kernel/src/DirectoryValidator.cpp
     ../mantid/Framework/Kernel/src/DiskBuffer.cpp
     ../mantid/Framework/Kernel/src/DllOpen.cpp
     ../mantid/Framework/Kernel/src/EnabledWhenProperty.cpp
     ../mantid/Framework/Kernel/src/EnvironmentHistory.cpp
+#   ../mantid/Framework/Kernel/src/EqualBinsChecker.cpp
+#   ../mantid/Framework/Kernel/src/ErrorReporter.cpp
     ../mantid/Framework/Kernel/src/Exception.cpp
     ../mantid/Framework/Kernel/src/FacilityInfo.cpp
     ../mantid/Framework/Kernel/src/FileDescriptor.cpp
     ../mantid/Framework/Kernel/src/FileValidator.cpp
     ../mantid/Framework/Kernel/src/FilteredTimeSeriesProperty.cpp
     ../mantid/Framework/Kernel/src/FloatingPointComparison.cpp
+#   ../mantid/Framework/Kernel/src/FreeBlock.cpp
+#   ../mantid/Framework/Kernel/src/GitHubApiHelper.cpp
     ../mantid/Framework/Kernel/src/Glob.cpp
     ../mantid/Framework/Kernel/src/ICatalogInfo.cpp
-    ../mantid/Framework/Kernel/src/InstrumentInfo.cpp
-    ../mantid/Framework/Kernel/src/Interpolation.cpp
     ../mantid/Framework/Kernel/src/IPropertyManager.cpp
     ../mantid/Framework/Kernel/src/ISaveable.cpp
+#   ../mantid/Framework/Kernel/src/IndexSet.cpp
+    ../mantid/Framework/Kernel/src/InstrumentInfo.cpp
+#   ../mantid/Framework/Kernel/src/InternetHelper.cpp
+    ../mantid/Framework/Kernel/src/Interpolation.cpp
+#   ../mantid/Framework/Kernel/src/InvisibleProperty.cpp
     ../mantid/Framework/Kernel/src/LibraryManager.cpp
     ../mantid/Framework/Kernel/src/LibraryWrapper.cpp
     ../mantid/Framework/Kernel/src/LiveListenerInfo.cpp
-    ../mantid/Framework/Kernel/src/Logger.cpp
     ../mantid/Framework/Kernel/src/LogFilter.cpp
     ../mantid/Framework/Kernel/src/LogParser.cpp
-    ../mantid/Framework/Kernel/src/MandatoryValidator.cpp
-    MantidVersion.cpp
-    ../mantid/Framework/Kernel/src/Material.cpp
-    ../mantid/Framework/Kernel/src/MaterialBuilder.cpp
-    ../mantid/Framework/Kernel/src/Matrix.cpp
+    ../mantid/Framework/Kernel/src/Logger.cpp
+#   ../mantid/Framework/Kernel/src/MDAxisValidator.cpp
     ../mantid/Framework/Kernel/src/MDUnit.cpp
     ../mantid/Framework/Kernel/src/MDUnitFactory.cpp
-    ../mantid/Framework/Kernel/src/MersenneTwister.cpp
+#   ../mantid/Framework/Kernel/src/MagneticIon.cpp
+    ../mantid/Framework/Kernel/src/MandatoryValidator.cpp
+     MantidVersion.cpp
+#   ../mantid/Framework/Kernel/src/MaskedProperty.cpp
+    ../mantid/Framework/Kernel/src/Material.cpp
+    ../mantid/Framework/Kernel/src/MaterialBuilder.cpp
+#   ../mantid/Framework/Kernel/src/MaterialXMLParser.cpp
+#   ../mantid/Framework/Kernel/src/Math/ChebyshevPolyFit.cpp
+#   ../mantid/Framework/Kernel/src/Math/Distributions/ChebyshevPolynomial.cpp
+#   ../mantid/Framework/Kernel/src/Math/Distributions/ChebyshevSeries.cpp
+#   ../mantid/Framework/Kernel/src/Math/Optimization/SLSQPMinimizer.cpp
+    ../mantid/Framework/Kernel/src/Matrix.cpp
+#   ../mantid/Framework/Kernel/src/MatrixProperty.cpp
     ../mantid/Framework/Kernel/src/Memory.cpp
+    ../mantid/Framework/Kernel/src/MersenneTwister.cpp
     ../mantid/Framework/Kernel/src/MultiFileNameParser.cpp
     ../mantid/Framework/Kernel/src/MultiFileValidator.cpp
     ../mantid/Framework/Kernel/src/NDRandomNumberGenerator.cpp
@@ -65,30 +88,44 @@ set(KERNEL_SRC_FILES
     ../mantid/Framework/Kernel/src/PropertyNexus.cpp
     ../mantid/Framework/Kernel/src/PropertyWithValue.cpp
     ../mantid/Framework/Kernel/src/PropertyWithValueJSON.cpp
+#   ../mantid/Framework/Kernel/src/ProxyInfo.cpp
     ../mantid/Framework/Kernel/src/PseudoRandomNumberGenerator.cpp
     ../mantid/Framework/Kernel/src/Quat.cpp
+#   ../mantid/Framework/Kernel/src/ReadLock.cpp
     ../mantid/Framework/Kernel/src/RebinParamsValidator.cpp
     ../mantid/Framework/Kernel/src/RegexStrings.cpp
+#   ../mantid/Framework/Kernel/src/SetValueWhenProperty.cpp
     ../mantid/Framework/Kernel/src/SingletonHolder.cpp
+#   ../mantid/Framework/Kernel/src/SobolSequence.cpp
     ../mantid/Framework/Kernel/src/SplittingInterval.cpp
+#   ../mantid/Framework/Kernel/src/StartsWithValidator.cpp
     ../mantid/Framework/Kernel/src/Statistics.cpp
     ../mantid/Framework/Kernel/src/StdoutChannel.cpp
-    ../mantid/Framework/Kernel/src/Strings.cpp
     ../mantid/Framework/Kernel/src/StringContainsValidator.cpp
     ../mantid/Framework/Kernel/src/StringTokenizer.cpp
+    ../mantid/Framework/Kernel/src/Strings.cpp
+#   ../mantid/Framework/Kernel/src/TestChannel.cpp
+#   ../mantid/Framework/Kernel/src/ThreadPool.cpp
+#   ../mantid/Framework/Kernel/src/ThreadPoolRunnable.cpp
     ../mantid/Framework/Kernel/src/ThreadSafeLogStream.cpp
     ../mantid/Framework/Kernel/src/TimeROI.cpp
-    ../mantid/Framework/Kernel/src/Timer.cpp
     ../mantid/Framework/Kernel/src/TimeSeriesProperty.cpp
+    ../mantid/Framework/Kernel/src/Timer.cpp
     ../mantid/Framework/Kernel/src/TopicInfo.cpp
     ../mantid/Framework/Kernel/src/Unit.cpp
     ../mantid/Framework/Kernel/src/UnitConversion.cpp
     ../mantid/Framework/Kernel/src/UnitLabel.cpp
     ../mantid/Framework/Kernel/src/UnitLabelTypes.cpp
+#   ../mantid/Framework/Kernel/src/UsageService.cpp
+#   ../mantid/Framework/Kernel/src/UserCatalogInfo.cpp
+#   ../mantid/Framework/Kernel/src/UserStringParser.cpp
+#   ../mantid/Framework/Kernel/src/Utils.cpp
+#   ../mantid/Framework/Kernel/src/V2D.cpp
     ../mantid/Framework/Kernel/src/V3D.cpp
+    ../mantid/Framework/Kernel/src/VMD.cpp
     ../mantid/Framework/Kernel/src/VectorHelper.cpp
     ../mantid/Framework/Kernel/src/VisibleWhenProperty.cpp
-    ../mantid/Framework/Kernel/src/VMD.cpp
+#   ../mantid/Framework/Kernel/src/WriteLock.cpp
 )
 add_library(Kernel OBJECT ${KERNEL_SRC_FILES})
 target_include_directories(Kernel PRIVATE
