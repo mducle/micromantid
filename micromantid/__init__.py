7a9a6ed7aab24a20fb9ff5e1820fb25798c4da1d
Duc Le <duc.le@stfc.ac.uk>
2025-04-17 21:54:26 +0100

    Add Mantid.properties. Re-enable more tests

--- micromantid/__init__.py
+++ micromantid/__init__.py
@@ -1,3 +1,6 @@
+import os
+os.environ['MANTIDPATH'] = os.path.dirname(__file__)
+
 from . import _micromantid
 from ._micromantid import _api, _kernel, _geometry, _dataobjects
 from . import kernel, api, geometry, dataobjects
