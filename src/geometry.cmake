578ee72052fa19c30be8ae3f648932ca8cabee9b
Duc Le <duc.le@stfc.ac.uk>
2025-05-10 01:18:47 +0100

    Try to make a C++ shared helper library
    Does not have any Boost::Python code
    But still fails with "getWasmTableEntry(...) is not a function"

--- src/geometry.cmake
+++ src/geometry.cmake
@@ -1,86 +1,86 @@
 set(GEOMETRY_SRC_FILES
 #   ../mantid/Framework/Geometry/src/ComponentParser.cpp
     ../mantid/Framework/Geometry/src/Crystal/BasicHKLFilters.cpp
-    ../mantid/Framework/Geometry/src/Crystal/BraggScatterer.cpp
-    ../mantid/Framework/Geometry/src/Crystal/BraggScattererFactory.cpp
-    ../mantid/Framework/Geometry/src/Crystal/BraggScattererInCrystalStructure.cpp
-    ../mantid/Framework/Geometry/src/Crystal/CenteringGroup.cpp
-    ../mantid/Framework/Geometry/src/Crystal/CompositeBraggScatterer.cpp
+   #../mantid/Framework/Geometry/src/Crystal/BraggScatterer.cpp
+   #../mantid/Framework/Geometry/src/Crystal/BraggScattererFactory.cpp
+   #../mantid/Framework/Geometry/src/Crystal/BraggScattererInCrystalStructure.cpp
+   #../mantid/Framework/Geometry/src/Crystal/CenteringGroup.cpp
+   #../mantid/Framework/Geometry/src/Crystal/CompositeBraggScatterer.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/ConventionalCell.cpp
-    ../mantid/Framework/Geometry/src/Crystal/CrystalStructure.cpp
-    ../mantid/Framework/Geometry/src/Crystal/CyclicGroup.cpp
+   #../mantid/Framework/Geometry/src/Crystal/CrystalStructure.cpp
+   #../mantid/Framework/Geometry/src/Crystal/CyclicGroup.cpp
     ../mantid/Framework/Geometry/src/Crystal/EdgePixel.cpp
-    ../mantid/Framework/Geometry/src/Crystal/Group.cpp
-    ../mantid/Framework/Geometry/src/Crystal/GroupTransformation.cpp
+   #../mantid/Framework/Geometry/src/Crystal/Group.cpp
+   #../mantid/Framework/Geometry/src/Crystal/GroupTransformation.cpp
     ../mantid/Framework/Geometry/src/Crystal/HKLFilter.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/HKLFilterWavelength.cpp
     ../mantid/Framework/Geometry/src/Crystal/HKLGenerator.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/IndexingUtils.cpp
-    ../mantid/Framework/Geometry/src/Crystal/IsotropicAtomBraggScatterer.cpp
+   #../mantid/Framework/Geometry/src/Crystal/IsotropicAtomBraggScatterer.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/NiggliCell.cpp
-    ../mantid/Framework/Geometry/src/Crystal/OrientedLattice.cpp
+   #../mantid/Framework/Geometry/src/Crystal/OrientedLattice.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/PeakTransform.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/PeakTransformHKL.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/PeakTransformQLab.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/PeakTransformQSample.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/PeakTransformSelector.cpp
-    ../mantid/Framework/Geometry/src/Crystal/PointGroup.cpp
-    ../mantid/Framework/Geometry/src/Crystal/PointGroupFactory.cpp
-    ../mantid/Framework/Geometry/src/Crystal/ProductOfCyclicGroups.cpp
+   #../mantid/Framework/Geometry/src/Crystal/PointGroup.cpp
+   #../mantid/Framework/Geometry/src/Crystal/PointGroupFactory.cpp
+   #../mantid/Framework/Geometry/src/Crystal/ProductOfCyclicGroups.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/ReducedCell.cpp
-    ../mantid/Framework/Geometry/src/Crystal/ReflectionCondition.cpp
+   #../mantid/Framework/Geometry/src/Crystal/ReflectionCondition.cpp
     ../mantid/Framework/Geometry/src/Crystal/ReflectionGenerator.cpp
 #   ../mantid/Framework/Geometry/src/Crystal/ScalarUtils.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SpaceGroup.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SpaceGroupFactory.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SpaceGroup.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SpaceGroupFactory.cpp
     ../mantid/Framework/Geometry/src/Crystal/StructureFactorCalculator.cpp
     ../mantid/Framework/Geometry/src/Crystal/StructureFactorCalculatorSummation.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SymmetryElement.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SymmetryElementFactory.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SymmetryOperation.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SymmetryOperationFactory.cpp
-    ../mantid/Framework/Geometry/src/Crystal/SymmetryOperationSymbolParser.cpp
-    ../mantid/Framework/Geometry/src/Crystal/UnitCell.cpp
-    ../mantid/Framework/Geometry/src/Crystal/V3R.cpp
-    ../mantid/Framework/Geometry/src/IObjComponent.cpp
-    ../mantid/Framework/Geometry/src/Instrument.cpp
-    ../mantid/Framework/Geometry/src/Instrument/CompAssembly.cpp
-    ../mantid/Framework/Geometry/src/Instrument/Component.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SymmetryElement.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SymmetryElementFactory.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SymmetryOperation.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SymmetryOperationFactory.cpp
+   #../mantid/Framework/Geometry/src/Crystal/SymmetryOperationSymbolParser.cpp
+   #../mantid/Framework/Geometry/src/Crystal/UnitCell.cpp
+   #../mantid/Framework/Geometry/src/Crystal/V3R.cpp
+   #../mantid/Framework/Geometry/src/IObjComponent.cpp
+   #../mantid/Framework/Geometry/src/Instrument.cpp
+   #../mantid/Framework/Geometry/src/Instrument/CompAssembly.cpp
+   #../mantid/Framework/Geometry/src/Instrument/Component.cpp
 #   ../mantid/Framework/Geometry/src/Instrument/ComponentHelper.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ComponentInfo.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ComponentInfoBankHelpers.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ComponentInfo.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ComponentInfoBankHelpers.cpp
 #   ../mantid/Framework/Geometry/src/Instrument/ComponentInfoIterator.cpp
     ../mantid/Framework/Geometry/src/Instrument/Container.cpp
-    ../mantid/Framework/Geometry/src/Instrument/DetectorInfo.cpp
-    ../mantid/Framework/Geometry/src/Instrument/DetectorGroup.cpp
-    ../mantid/Framework/Geometry/src/Instrument/Detector.cpp
-    ../mantid/Framework/Geometry/src/Instrument/FitParameter.cpp
-    ../mantid/Framework/Geometry/src/Instrument/Goniometer.cpp
-    ../mantid/Framework/Geometry/src/Instrument/GridDetector.cpp
-    ../mantid/Framework/Geometry/src/Instrument/GridDetectorPixel.cpp
-    ../mantid/Framework/Geometry/src/Instrument/IDFObject.cpp
-    ../mantid/Framework/Geometry/src/Instrument/InstrumentDefinitionParser.cpp
-    ../mantid/Framework/Geometry/src/Instrument/InstrumentVisitor.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ObjCompAssembly.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ObjComponent.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ParComponentFactory.cpp
-    ../mantid/Framework/Geometry/src/Instrument/Parameter.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ParameterMap.cpp
-    ../mantid/Framework/Geometry/src/Instrument/RectangularDetector.cpp
-    ../mantid/Framework/Geometry/src/Instrument/ReferenceFrame.cpp
+   #../mantid/Framework/Geometry/src/Instrument/DetectorInfo.cpp
+   #../mantid/Framework/Geometry/src/Instrument/DetectorGroup.cpp
+   #../mantid/Framework/Geometry/src/Instrument/Detector.cpp
+   #../mantid/Framework/Geometry/src/Instrument/FitParameter.cpp
+   #../mantid/Framework/Geometry/src/Instrument/Goniometer.cpp
+   #../mantid/Framework/Geometry/src/Instrument/GridDetector.cpp
+   #../mantid/Framework/Geometry/src/Instrument/GridDetectorPixel.cpp
+   #../mantid/Framework/Geometry/src/Instrument/IDFObject.cpp
+   #../mantid/Framework/Geometry/src/Instrument/InstrumentDefinitionParser.cpp
+   #../mantid/Framework/Geometry/src/Instrument/InstrumentVisitor.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ObjCompAssembly.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ObjComponent.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ParComponentFactory.cpp
+   #../mantid/Framework/Geometry/src/Instrument/Parameter.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ParameterMap.cpp
+   #../mantid/Framework/Geometry/src/Instrument/RectangularDetector.cpp
+   #../mantid/Framework/Geometry/src/Instrument/ReferenceFrame.cpp
     ../mantid/Framework/Geometry/src/Instrument/SampleEnvironment.cpp
-    ../mantid/Framework/Geometry/src/Instrument/StructuredDetector.cpp
-    ../mantid/Framework/Geometry/src/Instrument/XMLInstrumentParameter.cpp
+   #../mantid/Framework/Geometry/src/Instrument/StructuredDetector.cpp
+   #../mantid/Framework/Geometry/src/Instrument/XMLInstrumentParameter.cpp
 #   ../mantid/Framework/Geometry/src/MDGeometry/CompositeImplicitFunction.cpp
-    ../mantid/Framework/Geometry/src/MDGeometry/GeneralFrame.cpp
+   #../mantid/Framework/Geometry/src/MDGeometry/GeneralFrame.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/HKL.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/IMDDimension.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/IMDDimensionFactory.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/MDBoxImplicitFunction.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/MDFrameFactory.cpp
-    ../mantid/Framework/Geometry/src/MDGeometry/MDGeometryXMLBuilder.cpp
+   #../mantid/Framework/Geometry/src/MDGeometry/MDGeometryXMLBuilder.cpp
 #   ../mantid/Framework/Geometry/src/MDGeometry/MDGeometryXMLParser.cpp
-    ../mantid/Framework/Geometry/src/MDGeometry/MDHistoDimension.cpp
+   #../mantid/Framework/Geometry/src/MDGeometry/MDHistoDimension.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/MDHistoDimensionBuilder.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/MDImplicitFunction.cpp
     ../mantid/Framework/Geometry/src/MDGeometry/MDPlane.cpp
@@ -94,42 +94,42 @@ set(GEOMETRY_SRC_FILES
 #   ../mantid/Framework/Geometry/src/Math/BnId.cpp
     ../mantid/Framework/Geometry/src/Math/ConvexPolygon.cpp
     ../mantid/Framework/Geometry/src/Math/ConvexPolygonIterator.cpp
-    ../mantid/Framework/Geometry/src/Math/PolyBase.cpp
+   #../mantid/Framework/Geometry/src/Math/PolyBase.cpp
     ../mantid/Framework/Geometry/src/Math/PolygonEdge.cpp
     ../mantid/Framework/Geometry/src/Math/PolygonIntersection.cpp
     ../mantid/Framework/Geometry/src/Math/Quadrilateral.cpp
 #   ../mantid/Framework/Geometry/src/Math/RotCounter.cpp
-    ../mantid/Framework/Geometry/src/Math/Triple.cpp
-    ../mantid/Framework/Geometry/src/Math/mathSupport.cpp
-    ../mantid/Framework/Geometry/src/Objects/BoundingBox.cpp
-    ../mantid/Framework/Geometry/src/Objects/CSGObject.cpp
+   #../mantid/Framework/Geometry/src/Math/Triple.cpp
+   #../mantid/Framework/Geometry/src/Math/mathSupport.cpp
+   #../mantid/Framework/Geometry/src/Objects/BoundingBox.cpp
+   #../mantid/Framework/Geometry/src/Objects/CSGObject.cpp
     ../mantid/Framework/Geometry/src/Objects/InstrumentRayTracer.cpp
-    ../mantid/Framework/Geometry/src/Objects/MeshObject.cpp
-    ../mantid/Framework/Geometry/src/Objects/MeshObject2D.cpp
-    ../mantid/Framework/Geometry/src/Objects/MeshObjectCommon.cpp
-    ../mantid/Framework/Geometry/src/Objects/Rules.cpp
-    ../mantid/Framework/Geometry/src/Objects/RuleItems.cpp
-    ../mantid/Framework/Geometry/src/Objects/ShapeFactory.cpp
-    ../mantid/Framework/Geometry/src/Objects/Track.cpp
-    ../mantid/Framework/Geometry/src/RandomPoint.cpp
+   #../mantid/Framework/Geometry/src/Objects/MeshObject.cpp
+   #../mantid/Framework/Geometry/src/Objects/MeshObject2D.cpp
+   #../mantid/Framework/Geometry/src/Objects/MeshObjectCommon.cpp
+   #../mantid/Framework/Geometry/src/Objects/Rules.cpp
+   #../mantid/Framework/Geometry/src/Objects/RuleItems.cpp
+   #../mantid/Framework/Geometry/src/Objects/ShapeFactory.cpp
+   #../mantid/Framework/Geometry/src/Objects/Track.cpp
+   #../mantid/Framework/Geometry/src/RandomPoint.cpp
     ../mantid/Framework/Geometry/src/Rasterize.cpp
-    ../mantid/Framework/Geometry/src/Rendering/GeometryHandler.cpp
-    ../mantid/Framework/Geometry/src/Rendering/GeometryTriangulator.cpp
-    ../mantid/Framework/Geometry/src/Rendering/ShapeInfo.cpp
-    ../mantid/Framework/Geometry/src/Rendering/vtkGeometryCacheReader.cpp
-    ../mantid/Framework/Geometry/src/Rendering/vtkGeometryCacheWriter.cpp
-    ../mantid/Framework/Geometry/src/Rendering/RenderingHelpersThrowing.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Cone.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Cylinder.cpp
+   #../mantid/Framework/Geometry/src/Rendering/GeometryHandler.cpp
+   #../mantid/Framework/Geometry/src/Rendering/GeometryTriangulator.cpp
+   #../mantid/Framework/Geometry/src/Rendering/ShapeInfo.cpp
+   #../mantid/Framework/Geometry/src/Rendering/vtkGeometryCacheReader.cpp
+   #../mantid/Framework/Geometry/src/Rendering/vtkGeometryCacheWriter.cpp
+   #../mantid/Framework/Geometry/src/Rendering/RenderingHelpersThrowing.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Cone.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Cylinder.cpp
 #   ../mantid/Framework/Geometry/src/Surfaces/General.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Line.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/LineIntersectVisit.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Plane.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Quadratic.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Sphere.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Surface.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Line.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/LineIntersectVisit.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Plane.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Quadratic.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Sphere.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Surface.cpp
 #   ../mantid/Framework/Geometry/src/Surfaces/SurfaceFactory.cpp
-    ../mantid/Framework/Geometry/src/Surfaces/Torus.cpp
+   #../mantid/Framework/Geometry/src/Surfaces/Torus.cpp
 )
 add_library(Geometry OBJECT ${GEOMETRY_SRC_FILES})
 target_include_directories(Geometry PRIVATE
--- src/index.cmake
+++ src/index.cmake
@@ -1,11 +1,11 @@
 set(INDEX_SRC_FILES
     ../mantid/Framework/Indexing/src/Extract.cpp
     ../mantid/Framework/Indexing/src/Group.cpp
-    ../mantid/Framework/Indexing/src/IndexInfo.cpp
+   #../mantid/Framework/Indexing/src/IndexInfo.cpp
     ../mantid/Framework/Indexing/src/LegacyConversion.cpp
-    ../mantid/Framework/Indexing/src/Partitioner.cpp
-    ../mantid/Framework/Indexing/src/RoundRobinPartitioner.cpp
-    ../mantid/Framework/Indexing/src/SpectrumNumberTranslator.cpp
+   #../mantid/Framework/Indexing/src/Partitioner.cpp
+   #../mantid/Framework/Indexing/src/RoundRobinPartitioner.cpp
+   #../mantid/Framework/Indexing/src/SpectrumNumberTranslator.cpp
 )
 add_library(Indexing OBJECT ${INDEX_SRC_FILES})
 target_include_directories(Indexing PRIVATE
