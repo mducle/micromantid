d8920b8de02099aa6c56c00d2332243bcd6f029f
Duc Le <duc.le@stfc.ac.uk>
2025-05-11 21:22:45 +0100

    Bugfix to get WorkspaceHistoryTest working

--- micromantid/__init__.py
+++ micromantid/__init__.py
@@ -4,7 +4,7 @@ os.environ['MANTIDPATH'] = os.path.dirname(__file__)
 from . import _micromantid
 from ._micromantid import _api, _kernel, _geometry, _dataobjects
 from . import kernel, api, geometry, dataobjects
-from .api import mtd
+from .api import mtd, FileFinder
 from .kernel import config
 
 def apiVersion():
